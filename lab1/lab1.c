0d8305320bb613c41c6a21e0058d895e10e8e27f
Emre Sever <emresever7@outlook.com>
2020-10-11 19:27:00 +0300

    Lab1 Exercises

---
+++ lab1/lab1.c
@@ -0,0 +1,7 @@
+//printing Hello World to the screen
+#include<stdio.h> //get input output
+int main() { //begins the program here
+	printf("Hello World!"); //to print something to the screen
+	return 0; //ended the program
+}
+//to run the program click ctrl and F5	
