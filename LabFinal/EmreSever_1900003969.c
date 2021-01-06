8c42b8a475e2d2b067ff0270f29e87c4e6ac9338
Emre Sever <emresever7@outlook.com>
2021-01-06 17:31:00 +0300

    Lab14 Exercises

---
+++ LabFinal/EmreSever_1900003969.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 10
+int main() {
+	int m[N][N];
+	printf("Enter size of the square matrix> ");
+	scanf_s("%d", &m);
+
+
+	return 0;
+}
+	 
+
+	
